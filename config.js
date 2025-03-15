2017a6bb45e40fc7f7bf58cddf83cfe53ffe1cef
deviltech6 <lokuashuu587@gmail.com>
2025-03-16 03:23:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "1sJwkBAA#IK71QZEVNioLk1MIPnb-Pyr6Iz67yKtbfuPx3NuSFOk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:nuBfkxuNdaDLFonEHoeKBqMGvWKtbsOJ@mainline.proxy.rlwy.net:39340",
   OWNER_NUM: process.env.OWNER_NUM || "94760091093",
 };
